baf37008f9dfc17521f5b076d0cbe56ae5c7d5ff
hant0 <hant8750@gmail.com>
2022-08-22 22:55:53 +0900

    Add function for window moving

--- init.el
+++ init.el
@@ -252,6 +252,12 @@
   :bind (("C-a" . mwim-beginning-of-code-or-line)
             ("C-e" . mwim-end-of-code-or-line)))
 
+;; windmove
+(global-set-key (kbd "C-c b")  'windmove-left)
+(global-set-key (kbd "C-c f") 'windmove-right)
+(global-set-key (kbd "C-c p")    'windmove-up)
+(global-set-key (kbd "C-c n")  'windmove-down)
+
 ;;End my additional writing setting;;;;;;;;;;;
 
 (provide 'init)
